0b230a510a82422eb313081c263835a22e6a8b06
unknown <sanskar@gmail.com>
2026-06-01 21:08:19 +0530

    fix: flip house icon, revert glass to original

--- js/components.js
+++ js/components.js
@@ -16,8 +16,8 @@ const HOUSE_SVG = `
 const LOGO_HTML = `
   <a href="index.html" class="nav-logo">
     <span class="logo-wordmark">
-      <span class="logo-stay" style="position:relative;display:inline-block">Stay<span style="position:absolute;top:-4px;left:80%;transform:translateX(-50%);width:20px;height:13px;line-height:0;display:block;color:inherit">${HOUSE_SVG}</span></span><span class="logo-dekh">Dekh</span>
-      <span class="logo-o" style="position:relative;top:4px;display:inline-block;transform:scaleX(-1)">${LOGO_SVG}</span>
+      <span class="logo-stay" style="position:relative;display:inline-block">Stay<span style="position:absolute;top:-4px;left:80%;transform:translateX(-50%) scaleX(-1);width:20px;height:13px;line-height:0;display:block;color:inherit">${HOUSE_SVG}</span></span><span class="logo-dekh">Dekh</span>
+      <span class="logo-o" style="position:relative;top:4px;display:inline-block">${LOGO_SVG}</span>
     </span>
   </a>`;
 
